1b029ab4e695d1e13131519ee1eaf4291c216de5
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-06-13 11:16:22 +0200

    FEAT : add burger menu tablet device

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() : void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
