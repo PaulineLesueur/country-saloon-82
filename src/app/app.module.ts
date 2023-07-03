aed23248713ababf21aee43cb487a43f179b4909
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-07-03 16:00:14 +0200

    FEAT : generate home component + switch home header in it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
