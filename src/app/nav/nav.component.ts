c31ceca86c152c806b6862ba7ffcf4db5f2d19eb
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-07-03 15:45:27 +0200

    REFACTOR : add sticky nav on scroll

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -10,5 +10,29 @@ export class NavComponent {
 
   toggleMenu() : void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  isSticky = false;
+  isLargeScreen = false;
+
+  ngOnInit() {
+    this.checkScreenWidth();
+  }
+
+  @HostListener('window:resize', ['$event']) onResize(event: Event) {
+    this.checkScreenWidth();
+  }
+
+  checkScreenWidth() {
+    this.isLargeScreen = window.innerWidth > 1180;
+  }
+
+  @HostListener('window:scroll', ['$event']) onScroll() {
+    const scrollPosition = window.scrollY;
+    if (this.isLargeScreen && scrollPosition >= 50) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
   }
 }
