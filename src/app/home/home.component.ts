55b53948c547f2f5f33ace576bb1c26d3bb535b8
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-07-24 13:49:05 +0200

    STYLE : add activities card style desktop device

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,19 +13,19 @@ export class HomeComponent implements OnInit {
     this.activitiesCard = [
       {
         title: 'Les événements du club',
-        description: 'Notre club organise régulièrement des événements publics où se retrouver entre passionnés de country.',
+        description: 'Notre club organise régulièrement des événements publics où se retrouver entre passionnés de country',
         imageUrl: '/assets/img/activities-img/event-img.jpg',
         pageUrl: '/evenements-du-club'
       },
       {
         title: 'Les cours',
-        description: 'Le coeur de notre association réside dans les cours de tout niveau que nous dispensons chaque semaine.',
+        description: 'Le coeur de notre association réside dans les cours de tous niveaux que nous dispensons chaque semaine',
         imageUrl: '/assets/img/activities-img/course-img.jpg',
         pageUrl: '/cours'
       },
       {
         title: 'Les danses',
-        description: 'Pour vous permettre de vous entrainer chez vous, nous mettons à votre disposition les fiches des danses apprises lors des cours.',
+        description: 'Pour vous permettre de vous entrainer chez vous, nous mettons à votre disposition les fiches des danses apprises lors des cours',
         imageUrl: '/assets/img/activities-img/dance-img.jpg',
         pageUrl: '/danses'
       }
