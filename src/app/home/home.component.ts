8471fff9b1538eab6de3572688997e14c0eb6813
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-07-24 11:58:36 +0200

    REFACTOR : add img sub folder + activities img

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,19 +14,19 @@ export class HomeComponent implements OnInit {
       {
         title: 'Les événements du club',
         description: 'Notre club organise régulièrement des événements publics où se retrouver entre passionnés de country.',
-        imageUrl: '',
+        imageUrl: '/assets/img/activities-img/event-img.jpg',
         pageUrl: '/evenements-du-club'
       },
       {
         title: 'Les cours',
         description: 'Le coeur de notre association réside dans les cours de tout niveau que nous dispensons chaque semaine.',
-        imageUrl: '',
+        imageUrl: '/assets/img/activities-img/course-img.jpg',
         pageUrl: '/cours'
       },
       {
         title: 'Les danses',
         description: 'Pour vous permettre de vous entrainer chez vous, nous mettons à votre disposition les fiches des danses apprises lors des cours.',
-        imageUrl: '',
+        imageUrl: '/assets/img/activities-img/dance-img.jpg',
         pageUrl: '/danses'
       }
     ];
