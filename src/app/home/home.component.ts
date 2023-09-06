f70fbe5b6599b2415fad6ec3c19260392a09ff37
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-09-06 12:31:59 +0200

    REFACTOR : incoming feature component content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
         title: 'Les événements du club',
         description: 'Notre club organise régulièrement des événements publics où se retrouver entre passionnés de country',
         imageUrl: '/assets/img/activities-img/event-img.jpg',
-        pageUrl: '/evenements-du-club'
+        pageUrl: '/coming-soon'
       },
       {
         title: 'Les cours',
@@ -28,7 +28,7 @@ export class HomeComponent implements OnInit {
         title: 'Les danses',
         description: 'Pour vous permettre de vous entrainer chez vous, nous mettons à votre disposition les fiches des danses apprises lors des cours',
         imageUrl: '/assets/img/activities-img/dance-img.jpg',
-        pageUrl: '/danses'
+        pageUrl: '/coming-soon'
       }
     ];
   }
