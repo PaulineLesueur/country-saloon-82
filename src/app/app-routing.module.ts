58f99470c7555f1f5397681c7997bd8936135dde
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-09-06 12:47:03 +0200

    FEAT : add routing for contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { BABaComponent } from './b-aba/b-aba.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { IncomingFeatureComponent } from './incoming-feature/incoming-feature.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'b-a-ba', component: BABaComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'coming-soon', component: IncomingFeatureComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
