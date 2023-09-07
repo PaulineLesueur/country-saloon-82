d9ce96d6302ad0593bc66ec9c6ec2435370a16fb
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-09-07 12:02:22 +0200

    FEAT : add routing for course page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { IncomingFeatureComponent } from './incoming-feature/incoming-feature.component';
 import { ContactComponent } from './contact/contact.component';
 import { MentionsLegalesComponent } from './mentions-legales/mentions-legales.component';
+import { CoursComponent } from './cours/cours.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'b-a-ba', component: BABaComponent },
+  { path: 'cours', component: CoursComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'mentions-legales', component: MentionsLegalesComponent },
   { path: 'coming-soon', component: IncomingFeatureComponent },
