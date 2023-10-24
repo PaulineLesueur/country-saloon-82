108b8952bc44b4eade0a8b7ef4cdc52267ac2dc9
Pauline Lesueur <paulinelesueur93@gmail.com>
2023-10-24 15:06:05 +0200

    REFACTOR : add event 28/29 oct

--- src/app/evenements-club/evenements-club.component.ts
+++ src/app/evenements-club/evenements-club.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivitiesCard } from '../models/activities-card.model';
 
 @Component({
   selector: 'app-evenements-club',
   templateUrl: './evenements-club.component.html',
   styleUrls: ['./evenements-club.component.scss']
 })
-export class EvenementsClubComponent {
+export class EvenementsClubComponent implements OnInit {
+  activitiesCard!: ActivitiesCard[];
 
+  ngOnInit(): void {
+    this.activitiesCard = [
+      {
+        title: 'Soirée & après midi dansante - 28/29 octobre 2023',
+        description: 'Salle des fêtes St Etienne-de-Tulmont',
+        imageUrl: '/assets/img/events-img/soiree2829Oct23.png',
+        pageUrl: '/28-29-octobre-2023-st-etienne-de-tulmont'
+      }
+    ];
+  }
 }
