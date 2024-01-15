c55412810a1710628ba475d59f2542394970be39
Scarounine <paulinelesueur93@gmail.com>
2024-01-15 09:39:49 +0100

    feat: happy new year card style

---
+++ src/app/happy-new-year/happy-new-year.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-happy-new-year',
+  templateUrl: './happy-new-year.component.html',
+  styleUrls: ['./happy-new-year.component.scss']
+})
+export class HappyNewYearComponent {
+
+}
