d70c95707d37804bd5dfd19f363b55721a45e3c2
Scarounine <paulinelesueur93@gmail.com>
2025-04-16 12:23:13 +0200

    festival 2025

---
+++ src/app/festival-sept/festival-sept.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SlideInterface } from '../types/slide.interface';
+
+@Component({
+  selector: 'app-festival-sept',
+  templateUrl: './festival-sept.component.html',
+  styleUrls: ['./festival-sept.component.scss']
+})
+export class FestivalSeptComponent {
+
+}
